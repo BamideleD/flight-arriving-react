1b19cdffa8fa7d637d623671196359944572907a
BamideleD <bamadboss@gmail.com>
2023-05-03 22:07:18 +0100

    Restyling the login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./App.module.css";
 import Login from "./Components/Login/Login";
+import Info from "./Components/Login/Info";
 
 
 const App = () => {
   return (
     <div className={styles.App}>
+      <Info />
       <Login />
     </div>
     
