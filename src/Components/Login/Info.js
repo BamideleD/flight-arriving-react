4257493a12d3d1584a082becf86941721d70c99b
BamideleD <bamadboss@gmail.com>
2023-05-05 20:16:54 +0100

    Uploading Background image

--- src/Components/Login/Info.js
+++ src/Components/Login/Info.js
@@ -6,7 +6,7 @@ const Info = () => {
     return (
         <div className={styles.info}>
             <div className={styles.header}>
-                <img src= 'https://drive.google.com/file/d/1yvDCb-HL3h3Qn8ChsKJUWSNNabydP8bE/view?usp=share_link'  alt= 'logo' width='50' height='50' />
+                <img src= 'https://github.com/BamideleD/flight-arriving-react/blob/master/public/logo.png?raw=true'  alt= 'logo' width='50' height='50' />
                 <h1>OPENSKY</h1>
             </div>
             
