ee9119f18f99412a3f3ea7367eb7cab4064fe128
BamideleD <bamadboss@gmail.com>
2023-05-10 20:52:31 +0100

    UI corrections

--- src/Components/Dashboard/SearchBox.js
+++ src/Components/Dashboard/SearchBox.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import DatePicker from 'react-datepicker';
+import {TimePicker} from 'react-ios-time-picker';
+
 
 import "react-datepicker/dist/react-datepicker.css";
 import styles from './SearchBox.module.css';
@@ -7,6 +9,7 @@ import styles from './SearchBox.module.css';
 const SearchBox = (props) => {
 
     const [startDate, setStartDate] = useState(new Date());
+    const [time, setTime] = useState('10:00')
 
     return (
         <div className={styles.overall}>
@@ -25,7 +28,7 @@ const SearchBox = (props) => {
                     </div>
 
                     <div className={styles.line4}>
-                        <div>
+                        <div className={styles.label}>
                             <label>Date</label>
                         </div>
                         <div className={styles.datePicker}>
@@ -44,29 +47,32 @@ const SearchBox = (props) => {
                     <div className={styles.line2}>
                         <div className={styles.input3}>
                             <label>Departure Airport</label>
-                            <input />
+                            <input type="text" id="Departure Airport" placeholder="Example: PDX or Portland International Airport"/>
                         </div>
                         <div className={styles.input3}>
                             <label>Arrival Airport</label>
-                            <input />
+                            <input type="text" id="Arrival Airport" placeholder="Example: PDX or Portland International Airport"/>
                         </div>
                     </div>
                     <div className={styles.line3}>
                         <div className={styles.input3}>
                             <label>Airline</label>
-                            <input />
+                            <input type="text" id="Airline" placeholder="Example: AA or American Airlines" />
                         </div>
                         <div className={styles.input3}>
                             <label>Time</label>
-                            <input />
+                            <div className={styles.timepicker}>
+                                <TimePicker value = {time} />
+                            </div>
+                            
                         </div>
                     </div>
                     <div className={styles.line4}>
-                        <div>
+                        <div className={styles.label}>
                             <label>Date</label>
                         </div>
-                        <div>
-                            <input />
+                        <div className={styles.datePicker}>
+                            <DatePicker dateFormat="dd/MM/yyyy" selected={startDate} />
                         </div>
                     </div>
                     <div>
